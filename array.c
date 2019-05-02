d8c7a6c8fba04ecaf867b808ccd1f2b670b74ece
SourceCodeOpen <50205043+SourceCodeOpen@users.noreply.github.com>
2019-05-02 21:37:16 +0800

    大苏打

---
+++ array.c
@@ -0,0 +1,14 @@
+#include "array.h"
+#include<stdio.h>
+Array array_creat(int init_size){
+	
+}
+void array_free(Array *a){
+}
+int array_size(const Array *a){
+}
+int* array_at(Array *a,int index){
+	
+}
+void array_inflate(Array *a,int more_size){
+}
